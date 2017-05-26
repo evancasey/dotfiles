3254cefeebe75784ace06f631df52821e706f974
evancasey <evan.casey92@gmail.com>
2017-05-26 19:01:02 -0400

    add install

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ cp $DOTFILES_HOME/.vimrc $HOME
 cp $DOTFILES_HOME/.zshrc $HOME
 
 wget https://github.com/VundleVim/Vundle.vim/archive/master.zip \
-  unzip -d ~/.vim/bundle/Vundle.vim
+  && unzip master.zip -d ~/.vim/bundle/Vundle.vim \
+  && rm master.zip
 
 vim +PluginInstall +qall
 
