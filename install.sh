e213e53658020b844bed646256351d9d458f36a0
evancasey <evan.casey92@gmail.com>
2017-05-29 15:04:58 -0400

    add solarized

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ wget https://github.com/evancasey/dotfiles/raw/master/.zshrc -P $HOME
 VUNDLE_HOME=$HOME/.vim/bundle
 
 wget -P $VUNDLE_HOME -O Vundle.vim https://github.com/VundleVim/Vundle.vim/archive/master.zip
-wget -P $VUNDLE_HOME -O vim-colors-solarized https://github.com/altercation/vim-colors-solarized/archive/master.zip
 
 cd $VUNDLE_HOME \
   && unzip Vundle.vim.zip \
@@ -17,6 +16,10 @@ cd $VUNDLE_HOME \
   && unzip vim-colors-solarized.zip \
   && mv vim-colors-solarized-master vim-colors-solarized.vim \
   && rm Vundle.vim.zip
+
+COLORS_HOME=$HOME/.vim/colors
+mkdir -p $COLORS_HOME
+wget https://github.com/evancasey/dotfiles/raw/master.solarized.vim -P $COLORS_HOME
 
 vim +PluginInstall +qall
 
